2d17c364a64c70b20ecedeff5ed8d29b0617f60e
XProject25 <xproject2025@outlook.com>
2025-11-18 00:56:24 +0100

    Fix API auth to return JSON instead of redirecting to login route

--- balkly-api/bootstrap/app.php
+++ balkly-api/bootstrap/app.php
@@ -24,6 +24,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Return JSON for unauthenticated API requests instead of redirect
+        $exceptions->renderable(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Unauthenticated.'
+                ], 401);
+            }
+        });
     })->create();
 
