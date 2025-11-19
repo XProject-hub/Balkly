e2c137edc087ef0070780c8960900ea68511153d
XProject25 <xproject2025@outlook.com>
2025-11-19 22:33:07 +0100

    Fix min_price and max_price filters to use filled() instead of has()

--- balkly-api/app/Http/Controllers/Api/ListingController.php
+++ balkly-api/app/Http/Controllers/Api/ListingController.php
@@ -30,11 +30,11 @@ class ListingController extends Controller
             $query->where('country', $request->country);
         }
 
-        if ($request->has('min_price')) {
+        if ($request->filled('min_price')) {
             $query->where('price', '>=', $request->min_price);
         }
 
-        if ($request->has('max_price')) {
+        if ($request->filled('max_price')) {
             $query->where('price', '<=', $request->max_price);
         }
 
