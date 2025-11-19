96abc000a3b9f2d6b6b7ef9d168627539d188799
XProject25 <xproject2025@outlook.com>
2025-11-19 22:57:29 +0100

    Fix Media model to support both owner and mediable relationships + add URL accessor

--- balkly-api/app/Models/Media.php
+++ balkly-api/app/Models/Media.php
@@ -10,16 +10,21 @@ class Media extends Model
     use HasFactory;
 
     protected $fillable = [
-        'owner_type',
-        'owner_id',
+        'mediable_type',
+        'mediable_id',
+        'file_name',
+        'file_path',
         'url',
         'type',
         'mime_type',
         'size',
         'order',
+        'collection_name',
         'ai_tags_json',
         'metadata',
     ];
+    
+    protected $appends = ['url'];
 
     protected function casts(): array
     {
@@ -32,6 +37,27 @@ class Media extends Model
     public function owner()
     {
         return $this->morphTo();
+    }
+    
+    public function mediable()
+    {
+        return $this->morphTo('mediable', 'mediable_type', 'mediable_id');
+    }
+    
+    /**
+     * Get full URL for the media file
+     */
+    public function getUrlAttribute()
+    {
+        if ($this->attributes['url'] ?? null) {
+            return $this->attributes['url'];
+        }
+        
+        if ($this->file_path) {
+            return url('storage/' . $this->file_path);
+        }
+        
+        return null;
     }
 }
 
