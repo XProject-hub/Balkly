0a1a765146ea8d1bcf825022befc65b31ca2eb70
XProject25 <xproject2025@outlook.com>
2026-01-31 14:05:21 +0100

    perf: add persistent MySQL connections to eliminate reconnect delays

--- balkly-api/config/database.php
+++ balkly-api/config/database.php
@@ -21,6 +21,8 @@ return [
             'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
     ],
